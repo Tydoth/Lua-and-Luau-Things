d5af04dd1c62e993510523363d0e898c382e7f2f
Tydoth <tydothy@gmail.com>
2025-12-18 13:23:53 +1000

    Enhance BindService with GC and retrieval features
    Updated the BindService module to include garbage collection for binds and added functionality to retrieve all binds. Adjusted constants and improved error handling.

--- things/BindService.lua
+++ things/BindService.lua
@@ -1,5 +1,4 @@
 --!strict
---!native
 --!optimize 2
 --[[
 	A module that doesn't use metatable magic or bindable events, pure table magic.
@@ -7,13 +6,21 @@
 	
 	made by Tydoth in like 30 minutes
 	it's literally impossible to make server to client communication without remotes
+	
+	TODO:
+	* Implement GC'd binds after a long pass (DONE)
+	* Implement a way to get all binds
 ]]
 
 -- private
 
 local CONSTANTS = {
-	LINK_LIMIT = 1024,
-	DEFAULT_ONCE = false
+	LINK_LIMIT      = 8, -- useful for preventing memory leaks 
+  DEFAULT_ONCE    = false,
+  DEFAULT_GC_MODE = false, -- garbage collected,
+  DEFAULT_GC_CLEAN_TIME = 300, -- time when gc'd binds are cleaned
+  DEFAULT_GC_CLEAN_INTERVAL = 1, -- clean gc'd binds after some time,
+  SERVICE_INIT_TAG = "ServitAlreadyRunningOrSomething"
 }
 
 local REASONS = {
@@ -23,7 +30,8 @@ local REASONS = {
 	NO_FUNCS_LINKED    = `[{script}] - Bind doesn't have links!`,
 	NOT_DISBANDABLE    = `[{script}] - Bind can't be disbanded!`,
 	BIND_EXISTS        = `[{script}] - Bind with name already exists!`,
-	LINK_LIMIT_REACHED = `[{script}] - Bind's link limit reached!`
+  LINK_LIMIT_REACHED = `[{script}] - Bind's link limit reached!`,
+  GC_BIND_NOT_FOUND  = `[{script} Internal] - Garbage Collected Bind not found.`
 }
 
 type LinkProps = {
@@ -38,7 +46,8 @@ type Link = {
 
 type BindObjectProps = {
 	can_disband : boolean?,
-	link_limit  : number?
+  link_limit  : number?,
+  decay_time  : number? -- automatically gives it to gc list if this arg is provided
 }
 
 type BindObject = {
@@ -49,10 +58,17 @@ type BindObject = {
 	name 				: string,
 	can_disband : boolean,
 	link_limit  : number,
-	link_amount : number
+  link_amount : number,
+  decay_time  : number?
 }
 
 local binds : {[string]: BindObject} = {}
+local garbage_collected_binds : {
+  [string]: {
+    name    : string,
+    gc_time : number
+  }
+} = {}
 
 local function findBindByName
 (
@@ -83,6 +99,69 @@ local function linkExists
 		return false
 	end
 	return true
+end
+
+local function triggerFunc
+(
+  bind_name : string,
+  link      : Link,
+  ...       : any
+): ()
+  
+  local func = link.func
+  if (not func) then
+    return
+  end	
+  local success, message = pcall(func, ...)
+  if (link.once) then
+    binds[bind_name].connections[link.name] = nil
+    binds[bind_name].link_amount -= 1
+  end
+  if (success) then
+    return
+  end
+  warn(`Error {message} encountered while triggering bind.`)
+end
+
+local function destroyGarbageCollectedBind
+(
+  name: string
+): ()
+  local found_correlating_bind = findBindByName(name)
+  if (not found_correlating_bind) then
+    warn(REASONS.GC_BIND_NOT_FOUND)
+    return
+  end
+  binds[name] = nil
+  garbage_collected_binds[name] = nil
+end
+
+local function updateGarbageCollectedBinds(): ()
+  local binds = garbage_collected_binds
+  for _, bind in binds do
+    bind.gc_time -= 1
+    if (bind.gc_time <= 0)  then
+     destroyGarbageCollectedBind(bind.name)
+    end
+  end
+end
+
+local function serviceLoop(): ()
+  while (true) do
+    task.wait(CONSTANTS.DEFAULT_GC_CLEAN_INTERVAL)
+    if (not next(garbage_collected_binds)) then
+      continue
+    end
+    updateGarbageCollectedBinds()
+  end
+end
+
+local function serviceInit(): ()
+  if (script:HasTag(CONSTANTS.SERVICE_INIT_TAG)) then
+    return
+  end
+  script:AddTag(CONSTANTS.SERVICE_INIT_TAG)
+  task.spawn(serviceLoop)
 end
 
 -- public
@@ -144,7 +223,7 @@ local function awaitTrigger
 		warn(REASONS.BIND_NOT_FOUND)
 		return
 	end
-	table.insert(bind.threads, coroutine.running())
+  table.insert(bind.threads, coroutine.running())
 
 	return coroutine.yield()
 end
@@ -193,21 +272,12 @@ local function triggerDeferred
 
 	local links = bind.connections
 	for _, link in links do
-		local func = link.func
-		if (not func) then
-			continue
-		end	
-		task.defer(function(...)
-			local success, message = pcall(func, ...)
-			if (link.once) then
-				bind.connections[link.name] = nil
-				bind.link_amount -= 1
-			end
-			if (success) then
-				return
-			end
-			warn(`Error {message} encountered while triggering bind.`)
-		end, ...)
+    task.defer(
+      triggerFunc,
+      bind.name,
+      link,
+      ...
+    )
 	end
 end
 
@@ -237,19 +307,9 @@ local function triggerBind
 
 	local links = bind.connections
 	for _, link in links do
-		local func = link.func
-		if (not func) then
-			continue
-		end
-		local success, message = pcall(func, ...)
-		if (link.once) then
-			bind.connections[link.name] = nil
-			bind.link_amount -= 1
-		end
-		if (success) then
-			continue
-		end
-		warn(`Error {message} encountered while triggering bind.`)
+    triggerFunc(
+      bind.name, link, ...
+    )
 	end
 end
 
@@ -307,12 +367,29 @@ local function newBind
 		name 				= bind_name,
 		can_disband = (can_disband or true),
 		link_limit  = link_limit,
-		link_amount = 0,
+    link_amount = 0,
+    decay_time  = (new_props.decay_time or nil),    
 		connections = {},
 		threads = {}
 	}
-	binds[bind_name] = bind
+  binds[bind_name] = bind
+  
+  if (bind.decay_time) then
+    garbage_collected_binds[bind_name] = {
+      name = bind_name,
+      gc_time = bind.decay_time
+    }
+  end
 end
+
+local function getAllBinds(): ({
+  [string] : BindObject
+  })
+  
+  return binds
+end
+
+serviceInit()
 
 return table.freeze(
 	{
@@ -324,6 +401,7 @@ return table.freeze(
 		cleanBind   	  = cleanBind,
 		bindFunction	  = bindFunction,
 		unbindFunction  = unbindFunction,
-		awaitTrigger    = awaitTrigger,
+ 	    awaitTrigger    = awaitTrigger,
+    	getAllBinds     = getAllBinds
 	}
 )
